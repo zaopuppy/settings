401dbc9bde04d1bfb0294d6e73efa5dc11228210
Yi Zhao <zhaoyi.zero@gmail.com>
2015-08-09 10:19:28 +0800

    1. add PyCharm project; 2. refact code as a multi-thread program, so we can implement auto-refresh

--- skeleton/python/ncurses/tetris/tetris.py
+++ skeleton/python/ncurses/tetris/tetris.py
@@ -3,10 +3,12 @@
 import curses
 import locale
 import random
+from threading import Thread, RLock
+import time
 
-g_screen = None
-g_win1 = None
-g_rot = 0
+
+# remember, `addstr` and `addch` are using different coordinates...
+
 
 BLOCK_DATA = {
     'O': (((1, 1, 0, 0), (1, 1, 0, 0), (1, 1, 0, 0), (1, 1, 0, 0)),
@@ -38,6 +40,7 @@ BLOCK_DATA = {
           ((0, 0, 0, 0), (7, 0, 0, 0), (0, 0, 0, 0), (7, 0, 0, 0)),
           ((0, 0, 0, 0), (7, 0, 0, 0), (0, 0, 0, 0), (7, 0, 0, 0))),
     }
+
 
 # rot = { 0, 1, 2, 3 }
 def get_data(name, rot):
@@ -73,7 +76,6 @@ def random_image(scr, width_start, width_end, height_start, height_end):
     for r in range(height_start, height_end):
         for c in range(width_start, width_end-2, 2):
             if random.randint(0, 1) == 1:
-                # remember, `addstr` and `addch` are using different coordinates...
                 scr.addstr(r, c, '  ', curses.A_REVERSE)
 
 
@@ -82,65 +84,108 @@ def draw_block(scr, row, col, name, rot):
     for r, line in enumerate(data):
         for c, mark in enumerate(line):
             if mark > 0:
-                scr.addstr(1+row+r, 1+col+c*2, '  ', curses.A_REVERSE)
+                scr.addstr(row+r, col+c*2, '  ', curses.A_REVERSE)
 
 
-def draw():
-    g_screen.clear()
-    g_screen.border()
-    g_win1.border()
-    draw_block(g_screen, 0, 0, 'T', g_rot)
+class CursesGame(Thread):
+    """
+    A game framework for curses
+    """
+    def __init__(self, main_screen):
+        super().__init__(
+            group=None, target=None,
+            name='curses-game',
+            args=(), kwargs=None, daemon=True)
+        self.main_screen = main_screen
+        self.height, self.width = main_screen.getmaxyx()
+        self.stop = False
+        self.fps = 0.0
+        self.need_redraw = True
+
+        self.event_queue_lock = RLock()
+        self.event_queue = []
+
+        # for current active block
+        self.cur_rot = 0
+
+        # win1 pos
+        win1_height = self.height
+        win1_width = divmod(self.width, 2)[0]-4
+        win1_pos_row = 0
+        win1_pos_col = 0
+        self.win1 = curses.newwin(
+            win1_height, win1_width,
+            win1_pos_row, win1_pos_col)
+
+        win2_height = self.height
+        win2_width = divmod(self.width, 2)[0]-4
+        win2_pos_row = 0
+        win2_pos_col = win1_pos_col + win1_width + 1
+        self.win2 = curses.newwin(
+            win2_height, win2_width,
+            win2_pos_row, win2_pos_col)
+
+        # hide cursor
+        curses.curs_set(0)
+        # curses.cbreak()
+        # self.main_screen.keypad(1)
+
+    def run(self):
+        delta = 0.0
+        last_time = time.time()
+        while not self.stop:
+            self.update(delta)
+            self.draw()
+            time.sleep(0.1)
+            l_time = time.time()
+            delta, last_time = (l_time - last_time), l_time
+            self.fps = 1/delta
+
+    def read_input(self):
+        ch = self.main_screen.getch()
+        with self.event_queue_lock:
+            self.event_queue.append(ch)
+        self.need_redraw = True
+
+    def update(self, delta):
+        # print('delta={}, fps={}'.format(delta, self.fps))
+        with self.event_queue_lock:
+            event_queue = self.event_queue.copy()
+            self.event_queue.clear()
+        for ev in event_queue:
+            if ev == ord(' '):
+                self.cur_rot = (self.cur_rot+1) % 4
+            elif ev == ord('q'):
+                self.stop = True
+        pass
+
+    def draw(self):
+        if not self.need_redraw:
+            return
+        self.need_redraw = False
+
+        try:
+            # self.main_screen.clear()
+            # self.main_screen.border()
+            self.win1.clear()
+            self.win1.border()
+            self.win2.clear()
+            self.win2.border()
+            draw_block(self.win1, 1+5, 1+5, 'T', self.cur_rot)
+        finally:
+            self.main_screen.refresh()
+            self.win1.refresh()
+            self.win2.refresh()
 
 
-def main_old():
-    global g_screen, g_rot
-    global g_win1
-
-    g_screen = init()
-    try:
-        # (55, 178), y = height, x = width
-        height, width = g_screen.getmaxyx()
-        height, width = height-1, width-1
-
-        g_win1 = curses.newwin(10, 10, 20, 20)
-        g_win1.refresh()
-
-        while True:
-            draw()
-            ch = g_screen.getch()
-            if ch == ord('q'):
-                break
-            elif ch == ord(' '):
-                _, g_rot = divmod(g_rot+1, 4)
-    finally:
-        clear_and_quit(g_screen);
-
-
-def main(stdscr):
-    global g_screen, g_rot
-    global g_win1
-
-    curses.curs_set(0)
-
-    g_screen = stdscr
-
-    # (55, 178), y = height, x = width
-    height, width = g_screen.getmaxyx()
-    height, width = height-1, width-1
-
-    g_win1 = curses.newwin(10, 10, 20, 20)
-    g_win1.refresh()
-
-    while True:
-        draw()
-        ch = g_screen.getch()
-        if ch == ord('q'):
-            break
-        elif ch == ord(' '):
-            _, g_rot = divmod(g_rot+1, 4)
+def main(screen):
+    game = CursesGame(screen)
+    game.start()
+    while not game.stop:
+        game.read_input()
 
 
 if __name__ == '__main__':
     curses.wrapper(main)
 
-
+# END
