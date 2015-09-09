061f15f2e09dce3ad8c38fae11eff0f759c4beb7
Yi Zhao <zhaoyi.zero@gmail.com>
2015-09-09 20:29:52 +0800

    remove useless parsing of page

--- skeleton/crawler/little/crawler/crawler/spiders/little_spider.py
+++ skeleton/crawler/little/crawler/crawler/spiders/little_spider.py
@@ -1,23 +1,59 @@
 #!/usr/bin/env python
 
-import scrapy
-from scrapy.http.response.html import HtmlResponse
-from scrapy.spiders import CrawlSpider
 
-class LittleSpider(scrapy.Spider):
-    name = "little"
-    allowed_domains = ["www.vim.org"]
+from scrapy.http.response.html import HtmlResponse
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from crawler.items import LittleItem
+
+
+class LittleSpider(CrawlSpider):
+    name = 'little'
+    allowed_domains = ['www.vim.org']
     start_urls = [
-            "http://www.vim.org/",
+            'http://www.vim.org/',
             ]
-    def parse(self, response):
-        print('type: {}'.format(type(response)))
-        print(response.url)
+
+    rules = (
+            Rule(LxmlLinkExtractor(restrict_xpaths=('//a',)),
+                callback="parse_items",
+                follow=True),
+            )
+
+    def parse_items(self, response):
         if not isinstance(response, HtmlResponse):
             return
-        for href in response.xpath('//a/@href'):
-            url = response.urljoin(href.extract())
-            print('url: {}'.format(url))
-            yield scrapy.Request(url, callback=self.parse)
+        item = LittleItem()
+        item['link'] = response.url
+        return [item]
+        # item_list = []
+        # for href in response.xpath('//a/@href'):
+        #     url = response.urljoin(href.extract())
+        #     # print(url)
+        #     item = LittleItem()
+        #     item['link'] = url
+        #     item_list.append(item)
+        # return item_list
+
+# class LittleSpider(scrapy.Spider):
+#     name = "little"
+#     allowed_domains = ["www.vim.org"]
+#     start_urls = [
+#             "http://www.vim.org/",
+#             ]
+#     def parse(self, response):
+#         # item = LittleItem()
+#         # item['link'] = response.url
+#         # yield item
+#         if not isinstance(response, HtmlResponse):
+#             return
+#         for href in response.xpath('//a/@href'):
+#             url = response.urljoin(href.extract())
+#             item = LittleItem()
+#             item['link'] = url
+#             yield item
+#             yield scrapy.Request(url, callback=self.parse)
+# 
 
 
